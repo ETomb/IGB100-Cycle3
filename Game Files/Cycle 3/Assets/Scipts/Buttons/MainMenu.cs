07d8294a700ee0e82d4b7fe04da40020d1f1baaf
SamuelWeir <37795290+SamuelWeir@users.noreply.github.com>
2018-06-03 07:46:47 +1000

    Sounds + Menu Update

---
+++ Game Files/Cycle 3/Assets/Scipts/Buttons/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void Start() {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void PlayGame ()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void Tutorial ()
+	{
+		SceneManager.LoadScene(2);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+
+	}
+
+
+}
